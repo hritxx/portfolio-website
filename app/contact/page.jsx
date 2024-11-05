cfe1994b97ac54e69a634c61bbd71f0a72cd310b
hritxx <hriteekroy1869@gmail.com>
2024-11-05 20:15:31 +0530

    fix typo

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -109,7 +109,7 @@ const Contact = () => {
               })}
               <div className="mb-8 xl:mb-0">
                 <Social
-                  contsinerStyles="flex gap-6"
+                  containerStyles="flex gap-6"
                   iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500 "
                 />
               </div>
